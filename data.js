c4cbb7893853aaf6fbfa211eeb61110fda4ba433
21905036 <21905036@unicaen.fr>
2021-03-19 13:33:05 +0100

    Pudate imgs

--- data.js
+++ data.js
@@ -1,15 +1,15 @@
 let data = [
   {"titre":"Projet 1",
   "lore":"Description glob'ale du projet !",
-  "img1":"https://scriptverse.academy/img/tutorials/js-add-remove-class.png",
-  "img2":"",
-  "img3":"",
+  "img1":"images/projet1/i1.png",
+  "img2":"images/projet1/i2.png",
+  "img3":"images/projet1/i3.png",
   "img4":""},
 
   {"titre":"Projet 2",
   "lore":"Description globale du projet !",
-  "img1":"https://cdn.lynda.com/course/718674/718674-637491221301362093-16x9.jpg",
-  "img2":"https://scriptverse.academy/img/tutorials/js-add-remove-class.png",
+  "img1":"https://scriptverse.academy/img/tutorials/js-add-remove-class.png",
+  "img2":"https://cdn.lynda.com/course/718674/718674-637491221301362093-16x9.jpg",
   "img3":"",
   "img4":""},
 
